5d95943d0fb13eb97f21ca9a5776023800059aa9
APhan <APhan@172.17.67.84>
2018-04-24 21:33:59 -0500

    ReadFile

--- src/edu/uiowa/cs/similarity/readFile.java
+++ src/edu/uiowa/cs/similarity/readFile.java
@@ -15,22 +15,14 @@ import java.util.ArrayList;
  */
 public class readFile {
     public static void readFileWithScanner(String fileName) throws FileNotFoundException{
-        Scanner file = new Scanner(new File(fileName));
-        String [] sentence;
+        String str = "Hello World";
+        Scanner file = new Scanner(str);
         ArrayList<String> sentenceList = new ArrayList<String>();
         //file.useDelimiter(",*--:;")// for cleaning up words
         while (file.hasNext()){
-            String string = file.nextLine();
-            System.out.println(string);
-        }
-        file.close();
-        String[] sentenceToArray = sentenceList.toArray(new String[sentenceList.size()]);
-        for (int i = 0 ; i < sentenceToArray.length; i++){
-            sentence = sentenceToArray[i].split(".!?");
-            for (int j = 0; j<sentence.length;j++){
-                System.out.println("Sentence" + (j+1)+" : " + sentence[j]);
-            }
-        }   
+            sentenceList.add(file.nextLine());
+       
+    }
     }
 }
 
