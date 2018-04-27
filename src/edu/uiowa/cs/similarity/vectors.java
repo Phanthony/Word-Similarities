ea656e9efe25489e456c2f8225bc7b3276161d27
APhan <APhan@172.17.67.84>
2018-04-26 19:53:43 -0500

    Vectors starting 2

--- src/edu/uiowa/cs/similarity/vectors.java
+++ src/edu/uiowa/cs/similarity/vectors.java
@@ -10,12 +10,46 @@ import java.util.*;
 
 
 public class vectors {
-    public static Map vectorCreation(Map<String, Map> masterMap , ArrayList<ArrayList<String>> sentence){
+    public static Map vectorCreation(Map<String, Map> master , ArrayList<ArrayList<String>> sentence){
       for (ArrayList<String> sentenceArray : sentence){
+          ArrayList<String> seen = new ArrayList<>();
           for (String word : sentenceArray){
-              String current = word;
-              
+              if(!inside(word, seen)){
+              if (!master.containsKey(word)){
+                  Map <String, Double > vector = new HashMap<>();
+                  for(String word2 : sentenceArray){
+                      if(!word.equals(word2)){
+                          vector.put(word, 1.0);
+                      }
+                  }
+                  master.put(word, vector);
+                  seen.add(word);
+              }
+              Map<String, Double> insideVector = master.get(word);
+              for (String word2 : sentenceArray){
+                  if(!word.equals(word2)){
+                      if(insideVector.containsKey(word)){
+                          insideVector.replace(word, insideVector.get(word) + 1.0);
+                      }
+                      else{
+                          insideVector.put(word, 1.0);
+                      }
+                  }
+              }
           }
       }
     }
+      return master;
+    }
+    
+    private static boolean inside (String word, ArrayList<String> seen){
+        for (String checker : seen){
+            if(word.equals(checker)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
+
+
