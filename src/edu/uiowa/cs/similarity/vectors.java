6dbdc92f5a3faa4bf624ecb60b45b05dd4844a70
APhan <APhan@172.17.67.84>
2018-04-26 19:26:08 -0500

    Vectors starting

---
+++ src/edu/uiowa/cs/similarity/vectors.java
@@ -0,0 +1,21 @@
+package edu.uiowa.cs.similarity;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.ArrayList;
+import java.util.*;
+
+
+public class vectors {
+    public static Map vectorCreation(Map<String, Map> masterMap , ArrayList<ArrayList<String>> sentence){
+      for (ArrayList<String> sentenceArray : sentence){
+          for (String word : sentenceArray){
+              String current = word;
+              
+          }
+      }
+    }
+}
