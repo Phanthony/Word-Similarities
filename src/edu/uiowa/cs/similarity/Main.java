0e8e2a17bc264f1b4c975b0ba0cede9b24cd2e50
APhan <APhan@172.17.67.84>
2018-05-04 01:33:10 -0500

    Part 5 main touchup

--- src/edu/uiowa/cs/similarity/Main.java
+++ src/edu/uiowa/cs/similarity/Main.java
@@ -2,6 +2,7 @@ package edu.uiowa.cs.similarity;
 
 import java.io.*;
 import java.util.*;
+import opennlp.tools.stemmer.PorterStemmer;
 
 public class Main {
 	private static void printMenu() {
@@ -13,12 +14,16 @@ public class Main {
                 System.out.println("Sentences - Print the sentences in the file");
                 System.out.println("Vectors - Print the vectors for each word");
                 System.out.println("Topj WORD NUM - returns NUM number of words most similar to WORD");
+                System.out.println("Measure cos - change similarity measure to cosine similarity");
+                System.out.println("Measure euc - change similarity measure to negative euclidean distance");
+                System.out.println("Measure eucnorm - change similarity measure to negative euclidean distance between norms");
 	}
 
     public static void main(String[] args) throws IOException {
          ArrayList<ArrayList<String>> Index = new ArrayList<>();
          Map <String, Map<String, Double>> Vectors = new HashMap<>();
          CalculationFunction calc = new CosineSim();
+         PorterStemmer stem = new PorterStemmer();
 		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
 
 		while (true) {
@@ -46,7 +51,7 @@ public class Main {
                         else if (command.toLowerCase().startsWith("topj")){
                             String[] inputSplit = command.split(" ");
                             if (Vectors.containsKey(inputSplit[1])){
-                            System.out.println(vectors.topJ(Integer.parseInt(inputSplit[2]), inputSplit[1], Vectors, calc));
+                            System.out.println(vectors.topJ(Integer.parseInt(inputSplit[2]), stem.stem(inputSplit[1]), Vectors, calc));
                             }
                             else{
                                 System.out.println("Cannot Compute top-J similarity to " + inputSplit[1]);
@@ -63,6 +68,14 @@ public class Main {
                                 calc = new EucDistance();
                                 System.out.println("Similarity measure is negative euclidean distance");
                             }
+                            else if (inputSplit[1].toLowerCase().equals("eucnorm")){
+                                calc = new EucDistanceNorm();
+                                System.out.println("Similarity measure is negative euclidean distance between norms");
+                            }
+                            else{
+                                System.out.println("Similarity measure not found");
+                            }
+                            
                         }
                            
                         else if (command.toLowerCase().equals("num sentences")){
